2ac989580afd5c97232674f3e71a0ec185e905e3
Ramana Kumar <ramana.kumar@gmail.com>
2012-02-12 08:56:16 +0000

    alternative approach: spec as derived rule (along with sym and trans)

--- numeral/numconv.hs
+++ numeral/numconv.hs
@@ -20,17 +20,40 @@ rator (AppTerm (AppTerm f _) _) = f
 lhs (AppTerm (AppTerm _ l) _) = l
 rand (AppTerm _ r) = r
 rhs = rand
+truth = ConstTerm ((boolns,"T"),bool)
+alpha = VarType ([],"A")
+boolns = ["Data","Bool"]
+numns = ["Number","Natural"]
+fn x y = OpType ([],"->") [x, y]
+bool = OpType (boolns,"bool") []
+num = OpType (numns,"natural") []
+eq_tm ty = ConstTerm (([],"="), fn ty (fn ty bool))
+eq ty l r = AppTerm (AppTerm (eq_tm ty) l) r
+eqn = eq num
+forall_tm ty = (ConstTerm ((boolns,"!"), fn (fn ty bool) bool))
+forall v@(_,ty) bod = AppTerm (forall_tm ty) (AbsTerm v bod)
+forall_def = Axiom
+  (eq (fn (fn alpha bool) bool)
+    (forall_tm alpha)
+    (AbsTerm p
+      (eq (fn alpha bool)
+        (VarTerm p)
+        (AbsTerm x truth))))
+  where
+    x = (([],"x"),alpha)
+    p = (([],"P"),fn alpha bool)
 data Proof =
     Refl Term
-  | Spec Term Proof
   | AppThm Proof Proof
   | EqMp Proof Proof
   | Axiom Term
   | BetaConv Term
   deriving Show
+subst v t tm@(VarTerm v') = if v == v' then t else tm
+subst _ _ tm@(ConstTerm _) = tm
+subst v t (AppTerm t1 t2) = AppTerm (subst v t t1) (subst v t t2)
+subst v t tm@(AbsTerm v' b) = if v == v' then tm else AbsTerm v' (subst v t b)
 concl (Refl t) = eq (tyof t) t t
-concl (Spec t th) = subst v t tm
-  where (AbsTerm v tm) = rand (concl th)
 concl (AppThm th1 th2) = eq ty (AppTerm f1 x1) (AppTerm f2 x2)
   where (AppTerm (AppTerm _ f1) f2) = concl th1
         (AppTerm (AppTerm _ x1) x2) = concl th2
@@ -40,23 +63,28 @@ concl (Axiom t) = t
 concl (BetaConv (AppTerm (AbsTerm v tm) t)) = subst v t tm
 trans th1 th2 = EqMp (AppThm (Refl t) th2) th1
   where t = rator (concl th1)
-boolns = ["Data","Bool"]
-numns = ["Number","Natural"]
-fn x y = OpType ([],"->") [x, y]
-bool = OpType (boolns,"bool") []
-num = OpType (numns,"natural") []
-forall_tm ty = (ConstTerm ((boolns,"!"), fn (fn ty bool) bool))
-forall v@(_,ty) bod = AppTerm (forall_tm ty) (AbsTerm v bod)
-nv = (([],"n"),num)
-n = VarTerm nv
-eq ty l r =
-  AppTerm
-    (AppTerm (ConstTerm (([],"="), fn ty (fn ty bool))) l) r
-eqn = eq num
-subst v t tm@(VarTerm v') = if v == v' then t else tm
-subst _ _ tm@(ConstTerm _) = tm
-subst v t (AppTerm t1 t2) = AppTerm (subst v t t1) (subst v t t2)
-subst v t tm@(AbsTerm v' b) = if v == v' then tm else AbsTerm v' (subst v t b)
+sym th = EqMp lel_rel lel
+  where
+    lel = Refl l
+    lel_rel = AppThm ler el_el
+    el_el = Refl (AppTerm (eq_tm ty) l)
+    ty = tyof l
+    AppTerm (AppTerm _ l) r = concl th
+    ler = th
+spec tm th = EqMp (sym pv_T) (Axiom truth)
+  where
+    pv_T = trans pv_lxPxv (trans lxPxv_lxTv lxTv_T)
+    pv_lxPxv = sym (BetaConv lxPxv)
+    lxTv_T = BetaConv lxTv
+    AppTerm (AppTerm _ lxPxv) lxTv = concl lxPxv_lxTv
+    lxPxv_lxTv = AppThm lxPx_lxT (Refl v)
+    lxPx_lxT = BetaConv lPP_lxTlxPx
+    lPP_lxTlxPx = EqMp (AppThm fa_lPP_lxT (Refl lxPx)) fa_lxPx
+    lxPx = rand (concl th)
+    fa_lPP_lxT = forall_def ty
+    fa_lxPx = th
+    ty = tyof v
+    v = tm
 bit_tm s = ConstTerm ((numns,"bit"++s), fn num num)
 bit0_tm = bit_tm "0"
 bit1_tm = bit_tm "1"
@@ -65,6 +93,8 @@ bit1 = AppTerm bit1_tm
 bit2 = AppTerm (bit_tm "2")
 zero = ConstTerm ((numns,"zero"),num)
 suc = AppTerm (ConstTerm ((numns,"suc"), fn num num))
+nv = (([],"n"),num)
+n = VarTerm nv
 th1 = Axiom (forall nv (eqn (bit2 n) (suc (bit1 n))))
 th2 = Axiom (eqn (suc zero) (bit1 zero))
 th3 = Axiom (forall nv (eqn (suc (bit0 n)) (bit1 n)))
@@ -91,32 +121,15 @@ subs [] eq th = eq
 subs (R:rs) eq th = AppThm (Refl f) (subs rs eq th)
   where (AppTerm f _) = concl th
 binc BZero = th2
-binc (BBit0 n) = Spec (b2t n) th3
-binc (BBit1 n) = subs [R,R] (binc n) (Spec (b2t n) th4)
+binc (BBit0 n) = spec (b2t n) th3
+binc (BBit1 n) = subs [R,R] (binc n) (spec (b2t n) th4)
 n2b NZero = Refl zero
 n2b (NBit1 n) = AppThm (Refl bit1_tm) (n2b n)
-n2b (NBit2 n) = trans (subs [R,R,R] (n2b n) (Spec (n2t n) th1)) (binc nb)
+n2b (NBit2 n) = trans (subs [R,R,R] (n2b n) (spec (n2t n) th1)) (binc nb)
   where nb = t2b (rhs (concl (n2b n)))
 -- import Data.Map (Map)
 -- import qualified Data.Map as Map
 -- import Control.Monad.State
-truth = ConstTerm ((boolns,"T"),bool)
-alpha = VarType ([],"A")
-p ty = (([],"P"),fn ty bool)
-lxT ty = AbsTerm (([],"x"),ty) truth
-forall_def_tm ty = AbsTerm (p ty) (eq (fn ty bool) (VarTerm (p ty)) (lxT ty))
-forall_def = Axiom (eq (fn (fn alpha bool) bool) (forall_tm alpha) (forall_def_tm alpha))
-log_alpha_subst ty = do
-  --[[alpha,ty],[]]
-  log_nil
-  log_nil
-  log_cons
-  log_nil
-  log_type ty
-  log_cons
-  log_type alpha
-  log_cons
-  log_cons
 log_proof (Refl tm) = do
   log_term tm
   log_command "refl"
@@ -134,58 +147,3 @@ log_proof (AppThm th1 th2) = do
 log_proof (BetaConv tm) = do
   log_term tm
   log_command "betaConv"
-log_proof (Spec tm th) = do
-  c@(AbsTerm (_,ty) _) <- rand (concl th) -- c = \x. P x
-  log_proof (Refl tm)
-  -- |- v = v
-  log_proof (BetaConv (AppTerm (forall_def_tm ty) c))
-  -- |- (\P. P = \x. T) (\x. P x) = ((\x. P x) = \x. T)
-  -- |- v = v
-  log_alpha_subst ty
-  log_command "subst"
-  log_proof forall_def
-  -- |- (!) = \P. P = \x. T
-  -- |- (\P. P = \x. T) (\x. P x) = ((\x. P x) = \x. T)
-  -- |- v = v
-  log_proof (Refl c)
-  -- |- (\x. P x) = (\x. P x) 
-  -- |- (!) = \P. P = \x. T
-  -- |- (\P. P = \x. T) (\x. P x) = ((\x. P x) = \x. T)
-  -- |- v = v
-  log_command "appThm"
-  -- |- (!) (\x. P x) = (\P. P = \x. T) (\x. P x)
-  -- |- (\P. P = \x. T) (\x. P x) = ((\x. P x) = \x. T)
-  -- |- v = v
-  log_proof th
-  -- |- (!) (\x. P x)
-  -- |- (!) (\x. P x) = (\P. P = \x. T) (\x. P x)
-  -- |- (\P. P = \x. T) (\x. P x) = ((\x. P x) = \x. T)
-  -- |- v = v
-  log_command "eqMp"
-  -- |- (\P. P = \x. T) (\x. P x)
-  -- |- (\P. P = \x. T) (\x. P x) = ((\x. P x) = \x. T)
-  -- |- v = v
-  log_command "eqMp"
-  -- |- (\x. P x) = \x. T
-  -- |- v = v
-  log_command "appThm"
-  -- |- (\x. P x) v = (\x. T) v
-  log_proof (BetaConv (AppTerm c tm))
-  -- |- (\x. P x) v = P v
-  -- |- (\x. P x) v = (\x. T) v
-  log_proof (BetaConv (AppTerm (lxT ty) tm))
-  -- |- (\x. T) v = T
-  -- |- (\x. P x) v = P v
-  -- |- (\x. P x) v = (\x. T) v
-  appThm to get
-  |- P v = T
-  refl to get
-  |- (=) = (=)
-  appThm to get
-  |- (=) P v = (=) T
-  appThm to get
-  |- P v = P v = T = P v
-  eqMp to get
-  |- T = P v
-  eqMp to get
-  |- P v
