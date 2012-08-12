812c1003ffe08cb41e5a0c5ed183985e7c62cdf2
Ramana Kumar <ramana.kumar@gmail.com>
2012-08-12 12:30:38 +0100

    fix depthConv to actually try the conversion
    previously was just recursing to subterms and failing at the leaves.
    also add more type annotations for conversions.

--- numeral/n2b.hs
+++ numeral/n2b.hs
@@ -508,16 +508,21 @@ readTerm = do
     _ -> readTerm
 
 type Conv = Term -> Either String Proof
+tryConv :: Conv -> Conv
 tryConv c tm = catchError (c tm) (const (return (Refl tm)))
+orElseConv :: Conv -> Conv -> Conv
 orElseConv c1 c2 tm = catchError (c1 tm) (const (c2 tm))
 depthConv :: Conv -> Conv
-depthConv = subConv . depthConv
+depthConv c = c `orElseConv` subConv (depthConv c)
+subConv :: Conv -> Conv
 subConv c = tryConv (appConv c `orElseConv` absConv c)
+appConv :: Conv -> Conv
 appConv c (AppTerm t1 t2) = do
   th1 <- tryConv c t1
   th2 <- tryConv c t2
   return (AppThm th1 th2)
 appConv c _ = throwError "appConv"
+absConv :: Conv -> Conv
 absConv c (AbsTerm v tm) = do
   th <- tryConv c tm
   return (AbsThm v th)
